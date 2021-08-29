e1aa1dbf1cc613dc144cdeb567c1487c3232950d
GHOST <ghostdevbusiness@gmail.com>
2021-08-29 11:18:33 +0100

    fix: commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import preprocess from 'svelte-preprocess';
 import svelte from 'rollup-plugin-svelte';
 import pkg from './package.json';
@@ -12,5 +13,6 @@ export default {
     plugins: [
         svelte({ preprocess: [preprocess()], emitCss: false }),
         resolve(),
+        commonjs(),
     ],
 };
