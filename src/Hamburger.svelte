bfc0e26876ab90054e7feb8652b0f79382c40ce9
GHOST <ghostdevbusiness@gmail.com>
2021-08-29 11:17:36 +0100

    feat: jsdoc annotations

--- src/Hamburger.svelte
+++ src/Hamburger.svelte
@@ -6,25 +6,61 @@
     // Required open boolean
     export let open;
 
-    // Type
+    /**
+     * Hamburger type
+     */
     export let type = 'spin';
 
-    // Color settings (We use the American spelling of "color" for simplicity as css uses that)
+    /**
+     * The base color of the hamburger
+     */
     export let color = 'black';
+
+    /**
+     * The color when the hamburger is active (defaults to the value of color)
+     * @type {string}
+     */
     export let activeColor = color;
 
-    // Padding
+    /**
+     * Padding left and right of the hamburger
+     */
     export let paddingX = '12px';
+
+    /**
+     * Padding top and bottom of the hamburger
+     */
     export let paddingY = '12px';
 
-    // Layer settings
+    /**
+     * The width of each layer of the hamburger
+     */
     export let layerWidth = '30px';
+
+    /**
+     * The height of each layer of the hamburger
+     */
     export let layerHeight = '4px';
+
+    /**
+     * The spacing between each layer of the hamburger
+     */
     export let layerSpacing = '6px';
+
+    /**
+     * The border radius of each layer of the hamburger
+     */
     export let borderRadius = '4px';
 
-    // Hover filtering
+    /**
+     * The amount of opacity when you hover over the hamburger
+     */
     export let hoverOpacity = 0.7;
+
+    /**
+     * The amount of opacity when the hamburger is active (defaults to the value of hoverOpacity)
+     * @type {number}
+     */
     export let activeHoverOpacity = hoverOpacity;
 
     // Map inputs to css variables
