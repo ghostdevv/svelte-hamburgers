015476da515868d236b49c7c27aae2ac4d6576dd
GHOST <ghostdevbusiness@gmail.com>
2023-06-11 17:48:11 +0000

    chore: update packaging

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,12 +9,6 @@ const config = {
 
     kit: {
         adapter: adapter(),
-    },
-
-    package: {
-        exports: (file) => {
-            return file === 'index.ts';
-        },
     },
 };
 
